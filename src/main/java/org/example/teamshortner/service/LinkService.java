d091e4ad1b3c40f25b3ec55db538a350011acdb2
Mykola Klushyn <mklushyn@atlassian.com>
2024-04-25 07:00:15 +0200

    add properties

--- src/main/java/org/example/teamshortner/service/LinkService.java
+++ src/main/java/org/example/teamshortner/service/LinkService.java
@@ -1,9 +1,14 @@
 package org.example.teamshortner.service;
 
 import org.example.teamshortner.model.Link;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Map;
 
 public class LinkService {
+    @Value("${short-url.base-url}")
+    private String baseUrl;
+    @Value("${short-url.length}")
+    private String length;
     private Map<String, Link> context;
 }
