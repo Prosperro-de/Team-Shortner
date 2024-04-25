7e93164de10301a91bd01b3e460ff3d732a1dfd5
Kostiantyn Synytsia <20212876+martaisty@users.noreply.github.com>
2024-04-25 08:21:10 +0300

    Controller implementation

--- src/main/java/org/example/teamshortner/controller/MainController.java
+++ src/main/java/org/example/teamshortner/controller/MainController.java
@@ -1,4 +1,29 @@
 package org.example.teamshortner.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
 public class MainController {
+
+//    @Autowired
+//    LinkService linkService;
+
+    @PostMapping(value = "get-link", consumes = {MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> getLink(@RequestBody String baseUrl) {
+        // TODO call service
+        final var link = "test shortened url";
+        return ResponseEntity.ok(link);
+    }
+
+    @GetMapping("{uid}")
+    public RedirectView getOriginalLink(@PathVariable("uid") String uid) {
+        // TODO call service
+        final var originalUrl = "http://example.com/";
+        // TODO implement not found
+        return new RedirectView(originalUrl);
+    }
+
 }
